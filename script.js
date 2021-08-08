45b6882a519be05ccccc17e64da50d282c7edbd7
kibum99 <53718787+kibum99@users.noreply.github.com>
2021-08-08 19:22:25 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+function Monthchange(month){
+  const months = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+  months[1] = document.getElementById('jan');
+  months[2] = document.getElementById('feb');
+  months[3] = document.getElementById('mar');
+  months[4] = document.getElementById('apr');
+  months[5] = document.getElementById('may');
+  months[6] = document.getElementById('jun');
+  months[7] = document.getElementById('jul');
+  months[8] = document.getElementById('aug');
+  months[9] = document.getElementById('sep');
+  months[10] = document.getElementById('oct');
+  months[11] = document.getElementById('nov');
+  months[12] = document.getElementById('dec');
+  var current,i;
+  for(i=1;i<=12;i++){
+    if(!(months[i].classList.contains('notExist'))){
+      current = i;
+    }
+  }
+  if(month==13){
+    months[current].classList.add('notExist');
+    months[(current)%12+1].classList.remove('notExist');
+  }
+  else if(month==0){
+    months[current].classList.add('notExist');
+    months[(current+10)%12+1].classList.remove('notExist');
+  }
+  else{
+    months[current].classList.add('notExist');
+    months[month].classList.remove('notExist');
+  }
+}
+
+function ShowCalendar()  {
+  const cal = document.getElementById('calendar');
+  if(cal.classList.contains('invisible')){
+    cal.classList.remove('invisible');
+  }
+  else{
+    cal.classList.add('invisible');
+  }
+}
