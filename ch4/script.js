1e564aa2cf260e1a6b445bfedf1d785793c2665d
kibum99 <53718787+kibum99@users.noreply.github.com>
2021-09-09 19:07:36 +0900

    Update script.js

--- ch4/script.js
+++ ch4/script.js
@@ -1,5 +1,5 @@
 var total, coin, wallet, mycoin=[0,0,0];
-var costbit=10000, costether=10000, costdoge=10000;
+var costbit=500000, costether=20000, costdoge=1000;
 var delbit,delether,deldoge;
 var time=0, idxtime=0;
 var x=0;
@@ -9,7 +9,7 @@ $('document').ready(setInterval(function(){
   var newbit, newehter, newdoge;
 
   //bitcoin 갱신
-  newbit = costbit-10*Math.sin(idxtime/500)+500*Math.sin(0.02*time+0.05*idxtime)+100*Math.sin(0.3*time+0.9*idxtime)+5;
+  newbit = costbit-500*Math.sin(idxtime/500)+2500*Math.sin(0.02*time+0.05*idxtime)+5000*Math.sin(0.3*time+0.9*idxtime)+5;
   newbit = Math.ceil(newbit*100)/100;
   delbit = (newbit-costbit);
   $('#Item1 .cur').html(costbit);
@@ -18,7 +18,7 @@ $('document').ready(setInterval(function(){
   
 
   //ethereum 갱신
-  newether = costether-5*Math.sin(idxtime/500+2.1)+300*Math.sin(0.02*time+0.05*idxtime+2.1)+50*Math.sin(0.3*time+0.9*idxtime+2.1)+5;
+  newether = costether-10*Math.sin(idxtime/500+2.1)+600*Math.sin(0.02*time+0.05*idxtime+2.1)+100*Math.sin(0.3*time+0.9*idxtime+2.1)+10;
   newether = Math.ceil(newether*100)/100;
   delether = (newether-costether);
   $('#Item2 .cur').html(costether);
@@ -27,13 +27,13 @@ $('document').ready(setInterval(function(){
 
 
   //doge 갱신
-  newbit = costbit-10*Math.sin(idxtime/500)+500*Math.sin(0.02*time+0.05*idxtime)+100*Math.sin(0.3*time+0.9*idxtime)+5;
-  newbit = Math.ceil(newbit*100)/100;
-  delbit = (newbit-costbit);
-  $('#Item1 .cur').html(costbit);
-  $('#Item1 .del').html(Math.ceil(delbit*100)/100+"("+Math.ceil(delbit/costbit*10000)/100+"%)");
-  costbit = newbit;
-},1));
+  newdoge = costdoge+0.8*Math.sin(idxtime/500+4.2)-65*Math.sin(0.02*time+0.05*idxtime+4.2)+8*Math.sin(0.3*time+0.9*idxtime+4.2)+0.2;
+  newdoge = Math.ceil(newdoge*100)/100;
+  deldoge = (newdoge-costdoge);
+  $('#Item3 .cur').html(costdoge);
+  $('#Item3 .del').html(Math.ceil(deldoge*100)/100+"("+Math.ceil(deldoge/costdoge*10000)/100+"%)");
+  costdoge = newdoge;
+},500));
 
 
 function selectBnS(check){
